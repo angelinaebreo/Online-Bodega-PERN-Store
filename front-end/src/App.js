aff59e001c049e63a0fe9a8861f6523c307d2c49
Teyanna Earle <teyonna.earle92@gmail.com>
2021-07-20 19:19:35 -0400

    comment out app.js template code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,16 +14,16 @@ import "./App.css"
 const API = apiURL();
 
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => setDays(response.data),
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
+  // const [days, setDays] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get(`${API}/test`)
+  //     .then(
+  //       (response) => setDays(response.data),
+  //       (error) => console.log("get", error)
+  //     )
+  //     .catch((c) => console.warn("catch", c));
+  // }, []);
   
   return (
     <div className="app">
