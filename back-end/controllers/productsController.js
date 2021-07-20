6f82e9577cfc1031a58070a35fca55c400269530
Angelina Ebreo <angelinaebreo@gmail.com>
2021-07-20 17:03:16 -0400

    full CRUD functionality

--- back-end/controllers/productsController.js
+++ back-end/controllers/productsController.js
@@ -15,9 +15,7 @@ const db = require("../db/dbConfig.js");
 
 products.get("/", async (req, res) => {
   const allProducts = await getAllProducts();
-  console.log('request made to /products')
-  console.log(allProducts)
-  res.json(allProducts);
+  res.status(200).json(allProducts);
 });
 
 //show
@@ -26,7 +24,7 @@ products.get("/:id", async (req, res) => {
   try {
     const product = await getProduct(id);
     if (product.id) {
-      res.json(product);
+      res.status(200).json(product);
     } else {
       throw `NO item found at index: ${id}`;
     }
@@ -35,33 +33,36 @@ products.get("/:id", async (req, res) => {
   }
 });
 
-//create
-products.post("/", async (req, res, next) => {
-  try {
-    const product = await createProduct(req.body);
-    res.json(product);
-  } catch (e) {
-    return next(e);
-  }
-});
-
 // delete
 products.delete("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
     const deleted = await deleteProduct(id);
-    res.json(deleted);
+    res.status(200).json(deleted);
+  } catch (e) {
+    res.status(404).json({error: e})
+  }
+});
+
+
+//create
+products.post("/", async (req, res, next) => {
+  console.log(req.body)
+  try {
+    const product = await createProduct(req.body);
+    res.status(200).json(product);
   } catch (e) {
     return next(e);
   }
 });
+
 
 // update
 products.put("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
     const product = await updateProduct(id, req.body);
-    res.json(product);
+    res.status(200).json(product);
   } catch (e) {
     return next(e);
   }
