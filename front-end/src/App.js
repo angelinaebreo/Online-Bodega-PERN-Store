f1745f85467626a82a7dc02cca66b4293ce548b9
Jon Narine <jonnicwolf@gmail.com>
2021-07-19 21:27:42 -0400

    some components touched

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,28 +4,57 @@ import { apiURL } from "./util/apiURL.js";
 const API = apiURL();
 
 function App() {
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   //yesii
 
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    console.log("testing")
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => setDays(response.data),
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
-  return (
-    <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+//   const [days, setDays] = useState([]);
+//   useEffect(() => {
+//     console.log("testing")
+//     axios
+//       .get(`${API}/test`)
+//       .then(
+//         (response) => setDays(response.data),
+//         (error) => console.log("get", error)
+//       )
+//       .catch((c) => console.warn("catch", c));
+//   }, []);
+//   return (
+//     <div>
+//       <ul>
+//         {days.map((day) => (
+//           <li key={day.name}>{day.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default App;
