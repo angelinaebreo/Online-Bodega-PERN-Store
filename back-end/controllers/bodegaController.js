d7decf83d8cbd54168d4982487c57b0fe9525d01
Yesenia L <ylopezcr123@gmail.com>
2021-07-19 23:37:06 -0400

    show controller

--- back-end/controllers/bodegaController.js
+++ back-end/controllers/bodegaController.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const bodega = express.Router();
 const { getAll, getItem, newItem } = require("../queries/bodega")
+
+bodega.get("/", async (req, res) => {
+    const allItems = await getAll();
+    res.json(allBookmarks);
+});
+
+//show
+bodega.get("/:id", async (req,res) => {
+    const {id} = req.params;
+    try {
+        const item = await getAll(id);
+        if (item["id"]) {
+            res.json(item);
+        }else {
+            throw `NO item found at index: ${id}`
+        }
+    }catch (e){
+        res.status(404).json({err: 'not found', message: e});
+    }
+});
