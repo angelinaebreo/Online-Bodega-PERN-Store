8b0d1a5995ed2106c3aa6278f6987c4bc1a2c4a7
Teyanna Earle <teyonna.earle92@gmail.com>
2021-07-20 16:41:34 -0400

    Add category Links

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,13 +5,16 @@ import CategoryBanner from "./components/CategoryBanner.js";
 import Navbar from "./components/Navbar.js";
 import Header from "./components/Header.js";
 import HomePage from "./components/HomePage.js";
+import Bevs from "./components/Bevs";
+import Snacks from "./components/Snacks";
+import Sandwiches from "./components/Sandwiches";
+import Misc from "./components/Misc"
 import "./App.css"
+import { Route } from "react-router-dom";
 
 const API = apiURL();
 
 function App() {
-  //yesii
-
   const [days, setDays] = useState([]);
   useEffect(() => {
     axios
@@ -22,6 +25,8 @@ function App() {
       )
       .catch((c) => console.warn("catch", c));
   }, []);
+
+  
   return (
     <div className="app">
       <CategoryBanner />
@@ -36,6 +41,12 @@ function App() {
           <li key={day.name}>{day.name}</li>
         ))}
       </ul> */}
+
+
+      <Route path="/products/bevs" component={Bevs} />
+      <Route path="/products/snacks" component={Snacks} />
+      <Route path="/products/sandwiches" component={Sandwiches} />
+      <Route path="/products/misc" component={Misc} />
     </div>
   );
 }
---
+++ front-end/src/components/Sandwiches.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Product from './Product'
+
+function Sandwiches({products}) {
+    const sandwiches = products.filter(product => product.category === "Sandwich")
+    return (
+        <div>
+            <ul>
+                {sandwiches.map(sandwich => <li key={sandwich.id}> <Product product={sandwich}/> </li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default Sandwiches
